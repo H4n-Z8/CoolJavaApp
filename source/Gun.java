c60d23805a11273f7468331f2d140f222e2cd391
HanZ <nplaygames7@gmail.com>
2024-10-02 18:53:57 +0200

    Added Gun Class and used it in Main

---
+++ source/Gun.java
@@ -0,0 +1,91 @@
+public class Gun {
+    private String model;
+    private String manufacturer;
+    private int ammoCapacity;
+    private int currentAmmo;
+    private char firemode = 's';
+
+    public Gun (String model, String manufacturer, int ammoCapacity) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.ammoCapacity = ammoCapacity;
+        this.currentAmmo = ammoCapacity;
+    }
+
+
+    public String getModel() {
+        return this.model;
+    }
+    public String getManufacturer() {
+        return this.manufacturer;
+    }
+
+    public void setAmmoCapacity(int ammoCapacity) {
+        this.ammoCapacity = ammoCapacity;
+    }
+    public int getAmmoCapacity() {
+        return this.ammoCapacity;
+    }
+
+    public void setCurrentAmmo(int currentAmmo) {
+        this.currentAmmo = currentAmmo;
+    }
+    public int getCurrentAmmo() {
+        return this.currentAmmo;
+    }
+
+    public void reload() {
+        this.currentAmmo = this.ammoCapacity;
+        System.out.println("Gun has been Reloaded");
+    }
+
+     public void setFiremode(char firemode) {
+        this.firemode = firemode;  
+    }
+
+    public void switchFiremode() {
+        switch(this.firemode) {
+            case 's':
+                this.firemode = 'b';
+            break;
+            case 'b':
+                if (this.model == "Colt") {
+                    this.firemode = 's';
+                } else if (this.model == "Kalashnikov") {
+                    this.firemode = 'a';
+                }
+            break;
+            case 'a':
+                this.firemode = 's';
+            break;
+            default:
+                //placeholder
+        }  
+        System.out.println(this.firemode);      
+    }
+
+    public void shoot() {
+        if (this.currentAmmo < 0){
+            System.out.println("Youre out of ammo you have to Reload !!!");
+            return;
+        }
+
+        if (this.firemode == 's') {
+            this.currentAmmo = this.currentAmmo - 1;
+            System.out.println("Bang");
+            System.out.println("You have " + this.currentAmmo + " rounds left.");
+        } else if (this.firemode == 'b'){
+            this.currentAmmo = this.currentAmmo - 3;
+            System.out.println("Bang Bang Bang");
+            System.out.println("You have " + this.currentAmmo + " rounds left.");
+        } else if (this.firemode == 'a'){
+            while (this.currentAmmo > 0) {
+            this.currentAmmo = this.currentAmmo - 1;
+            System.out.println("Bang");
+            System.out.println("You have " + this.currentAmmo + " rounds left.");
+            }
+        } else {
+            System.out.println("Youre out of ammo you have to Reload !!!");
+        }
+    }
+}
--- source/Main.java
+++ source/Main.java
@@ -2,5 +2,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World! :3");
         System.out.println("Omg is that Hatune Miku?");
+
+        Gun G1 = new Gun("M4A1", "Colt", 30);
+        Gun G2 = new Gun("Ak12", "Kalashnikov", 60);
+
+        G2.shoot();
+        G2.switchFiremode();
+        G2.shoot();
+        G2.switchFiremode();
+        G2.shoot();
+
     }
 }
