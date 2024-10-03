2f7ebf2fa5e1500c1db13861c89f9d35d841a34d
HanZ <nplaygames7@gmail.com>
2024-10-03 12:56:06 +0200

    Replaced the if else statements for shoot with a switch

--- source/Gun.java
+++ source/Gun.java
@@ -54,27 +54,26 @@ public class Gun {
     }
 
     public void shoot() {
-        if (this.magazine.getCurrentAmmo() < 0){
-            System.out.println("Youre out of ammo you have to Reload !!!");
-            return;
-        }
-
-        if (this.firemode == 's') {
-            this.magazine.setCurrentAmmo(this.magazine.getCurrentAmmo() - 1);
-            System.out.println("Bang");
-            System.out.println("You have " + this.magazine.getCurrentAmmo() + " rounds left.");
-        } else if (this.firemode == 'b'){
-            this.magazine.setCurrentAmmo(this.magazine.getCurrentAmmo() - 3);
-            System.out.println("Bang Bang Bang");
-            System.out.println("You have " + this.magazine.getCurrentAmmo() + " rounds left.");
-        } else if (this.firemode == 'a'){
-            while (this.magazine.getCurrentAmmo() > 0) {
-            this.magazine.setCurrentAmmo(this.magazine.getCurrentAmmo() - 1);
-            System.out.println("Bang");
-            System.out.println("You have " + this.magazine.getCurrentAmmo() + " rounds left.");
-            }
-        } else {
-            System.out.println("Youre out of ammo you have to Reload !!!");
+        switch (this.firemode) {
+            case 's':
+                this.magazine.setCurrentAmmo(this.magazine.getCurrentAmmo() - 1);
+                System.out.println("Bang");
+                System.out.println("You have " + this.magazine.getCurrentAmmo() + " rounds left.");
+            break;
+            case 'b':
+                this.magazine.setCurrentAmmo(this.magazine.getCurrentAmmo() - 3);
+                System.out.println("Bang Bang Bang");
+                System.out.println("You have " + this.magazine.getCurrentAmmo() + " rounds left.");
+            break;
+            case 'a':
+                while (this.magazine.getCurrentAmmo() > 0) {
+                this.magazine.setCurrentAmmo(this.magazine.getCurrentAmmo() - 1);
+                System.out.println("Bang");
+                System.out.println("You have " + this.magazine.getCurrentAmmo() + " rounds left.");
+                }
+            break;
+            default:
+                System.out.println("Youre out of ammo you have to Reload !!!");
         }
     }
 }
